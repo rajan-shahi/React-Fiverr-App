55174f3e236a0aaff4d38a1779792d1fb25d0697
rajan <rajanshahi1267@gmail.com>
2023-08-12 09:20:19 -0700

    add Gigs page

---
+++ src/components/catCard/gigCard/GigCard.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./gigCard.scss"
+import { Link } from 'react-router-dom'
+
+export default function GigCard({item}) {
+  return (
+    <Link to ="/gig/123">
+    <div className="gigCard">
+        <img src={item.img} alt="" />
+        <div className="info">
+            <div className="user">
+                <img src={item.pp} alt="" />
+                <span>{item.username}</span>
+            </div>
+            <p>{item.desc}</p>
+            <div className="star">
+                <img src="./images/start.png" alt="" />
+                <span>{item.star}</span>
+            </div>
+        </div>
+        <hr />
+        <div className="details">
+            <img src="./images/heart.png" alt="" />
+            <div className="price">
+            <span>STARTING AT</span>
+            <h2>${item.price}</h2>
+            </div>
+        </div>
+    </div>
+    </Link>
+  )
+}
--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
-import "./gigs.scss"
+import React from "react";
+import "./gigs.scss";
 
 const Gigs = () => {
   return (
-    <div>
-      this is a gigs page
+    <div className="Gigs">
+      <div className="container">
+        <span className="breadcrumbs">FIVERR GRAPHICS & DESIGN</span>
+        <h1>AI Artists</h1>
+        <p>
+          Explore the boundaries of art and technology with Fiverr's AI artists
+        </p>
+        <div className="menu">
+          <div className="left">
+            <span>Budged</span>
+            <input type="text" placeholder="min" />
+            <input type="text" placeholder="max" />
+            <button>Apply</button>
+          </div>
+          <div className="right">
+            <span className="sortBy">sortBy</span>
+            <span className="sortType">
+              Best Selling
+            </span>
+            <img src="./images/down.png" alt="" />
+            <div className="rightMenu">
+              <span>Newest</span>
+              <span>Best Selling</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Gigs
+export default Gigs;
