cbd793a77458dab66e02b9a147af1a8af0152a58
rajan <rajanshahi1267@gmail.com>
2023-08-17 00:08:02 -0700

    commit

--- src/pages/add/Add.jsx
+++ src/pages/add/Add.jsx
@@ -1,12 +1,43 @@
-import React from 'react'
-import "./add.scss"
+import React from "react";
+import "./add.scss";
 
 const Add = () => {
   return (
-    <div>
-      Add
+    <div className="add">
+      <div className="container">
+        <h1>Add New Gig</h1>
+        <div className="section">
+          <div className="left">
+            <label htmlFor="">Title</label>
+            <input
+              type="text"
+              placeholder="e.g. I will do something I'm really good at"
+            />
+            <label htmlFor="">Category</label>
+            <select name="cats" id="cats">
+              <option value="Design">Design</option>
+              <option value="Web">web Development</option>
+              <option value="Animation">Animation</option>
+              <option value="music">Music</option>
+            </select>
+            <label htmlFor="">Cover Image</label>
+            <input type="file" />
+            <label htmlFor="">Upload Images</label>
+            <label htmlFor="">Description</label>
+            <textarea
+              name=""
+              id=""
+              cols="30"
+              rows="16"
+              placeholder="Brief description to introduce your service to customers"
+            ></textarea>
+            <button>Create</button>
+          </div>
+          <div className="right"></div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Add
+export default Add;
